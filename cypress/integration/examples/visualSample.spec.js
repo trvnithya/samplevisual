4bd329900ac26d4ce14f0b402551d834e99349ad
trvnithya <t.r.v.nithya@gmail.com>
2022-02-08 10:02:59 +0530

    Modified .gitignore

---
+++ cypress/integration/examples/visualSample.spec.js
@@ -0,0 +1,11 @@
+/// <reference types = 'cypress'/>
+describe('First Visual Testing Sample', () => {
+    it('Sample Snapshot Test', () => {
+        cy.visit('https://www.example.com')
+       cy.viewport('ipad-2')
+        //To Compare the screen
+       // cy.viewport('iphone-6')
+        cy.matchImageSnapshot()
+    })
+})
+  
