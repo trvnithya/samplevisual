f1598f9473d83d344d47d61c03f0cb498216a2b1
trvnithya <t.r.v.nithya@gmail.com>
2022-02-08 11:21:46 +0530

    Modfied Code to Pass

--- cypress/integration/examples/visualSample.spec.js
+++ cypress/integration/examples/visualSample.spec.js
@@ -2,9 +2,9 @@
 describe('First Visual Testing Sample', () => {
     it('Sample Snapshot Test', () => {
         cy.visit('https://www.example.com')
-       cy.viewport('ipad-2')
+       //cy.viewport('ipad-2')
         //To Compare the screen
-        //cy.viewport('iphone-6')
+        cy.viewport('iphone-6')
         cy.matchImageSnapshot()
     })
 })
